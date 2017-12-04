939e3f102fc3de2d7fe5f89c7fdc61abe060e1e7
André Serafim Pandolfi <aspandolfi@gmail.com>
2017-12-03 22:32:19 -0200

    Teste no azure

---
+++ src/server.ts
@@ -0,0 +1,36 @@
+import * as http from 'http';
+import 'reflect-metadata';
+import 'es6-shim';
+import { Container } from 'typedi';
+import { createConnection, useContainer } from 'typeorm';
+import { config } from './config/config';
+import {
+    BaseEntity,
+    User,
+    Cardapio,
+    Adicional,
+    Cliente,
+    Mesa,
+    Pedido,
+    ItemPedido,
+    ItemPedidoAdicional,
+    Produto,
+    ProdutoAdicionais,
+    TipoProduto,
+    Restaurante
+} from './entities';
+import { app } from './bin/app';
+
+useContainer(Container);
+
+const port = process.env.port || 8082;
+
+http.createServer(app).listen(port, () => console.log('Server started on port ' + port));
+
+// createConnection().then(
+//     () => http
+//         .createServer(app)
+//         .listen(port, () => console.log('Server started on port ' + port))
+// ).catch(
+//     (err: Error) => console.error(err.message)
+//     );
